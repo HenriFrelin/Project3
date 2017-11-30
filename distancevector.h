32c0f19a5657cf69d08786a36b459f96b87ee484
HenriFrelin <devhenri96@gmail.com>
2017-11-30 17:28:46 -0500

    Update distancevector.h

--- distancevector.h
+++ distancevector.h
@@ -1,1 +1,32 @@
+#ifndef _distancevector
+#define _distancevector
+#define Max_INT 21474783
+#include "node.h"
 
+class DistanceVector: public Node {
+    private:
+        // Anything you need in addition to Node members
+
+    public:
+        DistanceVector(unsigned, SimulationContext* , double, double);
+        DistanceVector(const DistanceVector &);
+        DistanceVector & operator=(const DistanceVector &);
+        ~DistanceVector();
+
+        // Inherited from Node
+        void LinkHasBeenUpdated(Link *l);
+        void ProcessIncomingRoutingMessage(RoutingMessage *m);
+        void TimeOut();
+        Node* GetNextHop(Node* destination);
+        Table* GetRoutingTable();
+        ostream & Print(ostream & os) const;
+
+        // Anything else
+	bool updateDV;
+};
+
+inline ostream & operator<<(ostream & os, const DistanceVector & n) {
+    return n.Print(os);
+}
+
+#endif
