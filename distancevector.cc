88794553076f208288f2cb658fe3b822214194d7
HenriFrelin <devhenri96@gmail.com>
2017-11-30 18:16:35 -0500

    Create distancevector.cc

---
+++ distancevector.cc
@@ -0,0 +1,53 @@
+#include "distancevector.h"
+#include "context.h"
+
+DistanceVector::DistanceVector(unsigned n, SimulationContext* c, double b, double l) :
+    Node(n, c, b, l)
+{}
+
+DistanceVector::DistanceVector(const DistanceVector & rhs) :
+    Node(rhs)
+{
+    *this = rhs;
+}
+
+DistanceVector & DistanceVector::operator=(const DistanceVector & rhs) {
+    Node::operator=(rhs);
+    return *this;
+}
+
+DistanceVector::~DistanceVector() {}
+
+
+/** Write the following functions.  They currently have dummy implementations **/
+void DistanceVector::LinkHasBeenUpdated(Link* l) {
+    cerr << *this << ": Link Update: " << *l << endl;
+    int destination = l.getDest();
+    int letency = l.getLatency();
+    routing_table.neighborLinks[destination].cost = latency;
+    routing_table.distanceVector[destination].cost = -1;
+    routing_table.topo[destination].cost = 0;
+
+    SendToNeighbors(new RoutingMessage());
+}
+
+void DistanceVector::ProcessIncomingRoutingMessage(RoutingMessage *m) {
+    cerr << *this << " got a routing message: " << *m << " (ignored)" << endl;
+}
+
+void DistanceVector::TimeOut() {
+    cerr << *this << " got a timeout: (ignored)" << endl;
+}
+
+Node* DistanceVector::GetNextHop(Node *destination) { 
+    return NULL;
+}
+
+Table* DistanceVector::GetRoutingTable() {
+    return NULL;
+}
+
+ostream & DistanceVector::Print(ostream &os) const { 
+    Node::Print(os);
+    return os;
+}
