9a452a52c0b50bae894c4dcf8b10ca7f0dbfb8f8
HenriFrelin <devhenri96@gmail.com>
2017-12-06 11:15:32 -0500

    Update table.cc

--- table.cc
+++ table.cc
@@ -30,18 +30,38 @@ ostream & Table::Print(ostream &os) const
 #if defined(LINKSTATE)
 ostream & Table::Print(ostream &os) const
 {
-  os << "LinkState Table()";
-  return os;
+	os << "LinkState Table()";
+	map<int,, map<int, TopoLink> >::const_iterator r = topo.begin();
+	while(r != topo.end()){
+		map<int, TopoLink>::const_iterator c = (*row).second.begin();
+		os << "SOURCE: " << (*r).first << std::endl;
+		while(c != (*r).second.end()){
+			os << "DESTINATION: " << (*s).first << std::endl;
+			c++;
+		}
+		r++;
+	}  
+  
+	return os;
 }
 #endif
 
 #if defined(DISTANCEVECTOR)
 ostream & Table::Print(ostream &os) const
 {
-  os << "DistanceVector Table()";
-  //map<int, TopoLink>
+	os << "DistanceVector Table()";
+	map<int, TopoLink>::const_iterator i = distanceVector.begin();
+	while(i !=  distanceVector.end()){
+		os << "NODE: " << (i*).first << "DISTANCE: " << (i*).second.cost << std::endl;
+		i++;
+	}	
+	os << "NEIGHBOR LIST: " << std::endl;
+	i = neighborLinks.begin();
+	while(i != neighborLinks.end()){
+		os << "NODE: " << (i*).first << "DISTANCE: " << (i*).second.cost << std::endl;
+		i++;
+	}	
 
-
-  return os;
+	return os;
 }
 #endif
